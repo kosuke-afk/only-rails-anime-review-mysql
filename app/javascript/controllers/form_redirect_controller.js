8f76e01c98a9c27b7a6a80ea40fcae94b60e2c4f
kosuke <irohalove2525@gmail.com>
2023-05-02 15:22:13 +0900

    modify redirect_to as turbo_stream

---
+++ app/javascript/controllers/form_redirect_controller.js
@@ -0,0 +1,18 @@
+import { Controller } from "@hotwired/stimulus";
+
+// Connects to data-controller="form-redirect"
+export default class extends Controller {
+  next(event) {
+    if (event.detail.success) {
+      const fetchResponse = event.detail.fetchResponse;
+
+      history.pushState(
+        { turbo_frame_history: true },
+        "",
+        fetchResponse.response.url
+      );
+
+      Turbo.visit(fetchResponse.response.url);
+    }
+  }
+}
