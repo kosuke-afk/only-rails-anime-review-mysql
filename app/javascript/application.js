333f0861b21dca5511724de0c98a423bbe550aff
kosuke <irohalove2525@gmail.com>
2023-05-02 11:01:00 +0900

    enable to redirect as turbo_stream request

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,17 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails";
 import "controllers";
+
+document.addEventListener("turbo:before-fetch-response", (event) => {
+  if (
+    event.detail.fetchResponse.header("X-Turbo-Connect") &&
+    typeof event.detail.fetchResponse !== "undefined"
+  ) {
+    var response = event.detail.fetchResponse.response;
+    if (response.redirected) {
+      console.log("need redirect");
+      event.preventDefault();
+      Turbo.visit(response.url, { action: "replace" });
+    }
+  }
+});
