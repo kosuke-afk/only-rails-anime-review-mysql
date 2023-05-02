be33883e3dc10b69e6b10f3c5ccf904991cf14a9
kosuke <irohalove2525@gmail.com>
2023-05-02 15:24:23 +0900

    change redirect_to for works-index as login_action

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -10,7 +10,7 @@ class SessionController < ApplicationController
       flash.now[:danger] = ["メールアドレスまたはパスワードが違います"]
       render turbo_stream: [
         turbo_stream.replace("error-area", partial: "home/partial/error_messages" )
-      ]
+      ],status: :unprocessable_entity
     end
   end
 
