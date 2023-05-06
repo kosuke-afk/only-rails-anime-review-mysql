151a814ea364c1c8d4210b7b6fc7b9c63107b433
kosuke <irohalove2525@gmail.com>
2023-05-06 11:31:24 +0900

    add register_method to release.rb

--- app/models/release.rb
+++ app/models/release.rb
@@ -1,2 +1,13 @@
 class Release < ApplicationRecord
+  has_many :works
+
+  def self.register(sYear,eYear)
+    (sYear..eYear).each do |year|
+      seasons = ["春","夏","秋","冬"]
+      seasons.each do |season|
+        Release.find_or_create_by(year: year,season: season)
+      end
+    end
+  end
+
 end
