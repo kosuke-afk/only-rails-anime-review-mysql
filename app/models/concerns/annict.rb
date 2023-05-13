8f3da2a80b1f772c668b017387485a127df2b863
kosuke <irohalove2525@gmail.com>
2023-05-13 19:04:22 +0900

    create register function to register annict_data for episodes_table

--- app/models/concerns/annict.rb
+++ app/models/concerns/annict.rb
@@ -8,11 +8,30 @@ module Annict
       JSON.parse(Faraday.get("#{base_url}/v1/works?per_page=50&filter_season=#{year}-#{season}&access_token=#{access_token}").body)
     end
 
-    def self.fetch_works(year:,season:,page:)
+    def self.fetch_works_each_page(year:,season:,page:)
       base_url = "https://api.annict.com"
       access_token = ENV['ANNICT_ACCESS_TOKEN']
       JSON.parse(Faraday.get("#{base_url}/v1/works?fields=title,media_text,images,season_name,episodes_count,id&per_page=50&filter_season=#{year}-#{season}&page=#{page}&sort_season=desc&access_token=#{access_token}").body)["works"]
     end
 
+    def self.fetch_work(title: nil,quantity: nil)
+      base_url = "https://api.annict.com"
+      access_token = ENV['ANNICT_ACCESS_TOKEN']
+      if title
+        title = URI.encode_www_form_component(title)
+        uri = "#{base_url}/v1/works?fields=title,media_text,images,season_name,episodes_count,id&filter_title=#{title}&access_token=#{access_token}"
+      elsif quantity
+        uri = "#{base_url}/v1/works?fields=title,media_text,images,season_name,episodes_count,id&per_page=#{quantity}&access_token=#{access_token}"
+      end
+      JSON.parse(Faraday.get(uri).body)["works"]
+    end
+  end
+
+  class Episode
+    def self.fetch_episodes(work)
+      base_url = "https://api.annict.com"
+      access_token = ENV['ANNICT_ACCESS_TOKEN']
+      JSON.parse(Faraday.get("#{base_url}/v1/episodes?fields=title,number_text,work.title,work.id&filter_work_id=#{work.annict_id}&sort_id=asc&access_token=#{access_token}").body)["episodes"]
+    end
   end
 end
--- app/models/episode.rb
+++ app/models/episode.rb
@@ -1,3 +1,14 @@
 class Episode < ApplicationRecord
   belongs_to :work, primary_key: :annict_id
+
+  def self.all_register
+    @works = Work.all
+    @works.each do |work|
+      episodes = Annict::Episode.fetch_episodes(work)
+      episodes.each do |episode|
+        Episode.find_or_create_by(title: episode["title"],work_id: episode["work"]["id"],episode_number: episode["number_text"])
+      end
+    end
+  end
+  
 end
