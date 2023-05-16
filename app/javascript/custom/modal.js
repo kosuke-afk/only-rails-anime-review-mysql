4a9836a59a24e5aca833cff26abe11e24e433344
kosuke <irohalove2525@gmail.com>
2023-05-16 17:24:20 +0900

    modified top-modal-layout

--- app/javascript/custom/modal.js
+++ app/javascript/custom/modal.js
@@ -1,43 +1,23 @@
 export default class Modal {
-  constructor(modal, openButton, closeButton, mask) {
+  constructor({ modal, openButton, className, mask = "top-modal__mask" }) {
     this.modal = document.getElementById(modal);
-    this.buttons = document.querySelectorAll(openButton);
-    this.closeButton = document.querySelector(closeButton);
-    this.mask = document.getElementById(mask) || null;
-    this._addOpenEvent(this.buttons);
-    this._addCloseEvent(this.closeButton);
-    if (this.mask != null) {
-      this._addCloseEvent(this.mask);
-    }
+    this.button = document.getElementById(openButton);
+    this.mask = document.getElementById(mask);
+    this._addOpenEvent(this.button, className);
+    this._addCloseEvent(this.mask);
   }
 
-  _addOpenEvent(doms) {
-    if (doms.length >= 2) {
-      doms.forEach((dom) => {
-        dom.addEventListener("click", () => {
-          this.modal.classList.remove("hidden");
-          if (this.mask != null) {
-            this.mask.classList.remove("hidden");
-          }
-        });
-      });
-    } else {
-      const dom = doms;
-      dom.addEventListener("click", () => {
-        this.modal.classList.remove("hidden");
-        if (this.mask != null) {
-          this.mask.classList.remove("hidden");
-        }
-      });
-    }
-  }
-
-  _addCloseEvent(dom) {
+  _addOpenEvent(dom, className) {
     dom.addEventListener("click", () => {
-      if (this.mask != null) {
-        this.mask.classList.add("hidden");
-      }
-      this.modal.classList.add("hidden");
+      this.modal.classList.add("open", className);
+      this.mask.classList.add("open");
+    });
+  }
+
+  addCloseEvent(dom) {
+    dom.addEventListener("click", () => {
+      this.modal.classList.remove(...this.modal.classList);
+      this.mask.classList.remove("open");
     });
   }
 }
---
+++ app/javascript/home/top.js
@@ -0,0 +1,12 @@
+import Modal from "../custom/modal";
+
+const createModal = new Modal({
+  modal: "modal",
+  openButton: "create",
+  className: "create",
+});
+const loginModal = new Modal({
+  modal: "modal",
+  openButton: "login",
+  className: "login",
+});
