3fe135ae6d811ca5e09bb61cad8ffe506aa06abe
kosuke <irohalove2525@gmail.com>
2023-06-07 13:27:42 +0900

    create slider function in user page

--- config/importmap.rb
+++ config/importmap.rb
@@ -10,3 +10,6 @@ pin "mobile_menu"
 pin_all_from "app/javascript/controllers", under: "controllers"
 
 
+pin "gsap", to: "https://ga.jspm.io/npm:gsap@3.11.5/index.js"
+pin "swiper", to: "https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.esm.browser.min.js"
+pin "ssr-window", to: "https://ga.jspm.io/npm:ssr-window@4.0.2/ssr-window.esm.js"
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get 'login_modal', to: "home#login_modal"
   post 'session/login'
   delete 'session/logout'
-  resources :users, only: [:edit,:create] do
+  resources :users, only: [:show,:edit,:create] do
     member do
       get ":work_id/episodes/:episode_id/open", to: "episode_rates#edit_open", as: "open_episode_rate"
       get ":work_id/episodes/:episode_id/close", to: "episode_rates#edit_close", as: "close_episode_rate"
@@ -12,9 +12,6 @@ Rails.application.routes.draw do
     end
   end
   resources :works, only: [:index,:show] do
-    member do
-      resources :work_rates, only: [:update]
-    end
     get 'search', on: :collection
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
