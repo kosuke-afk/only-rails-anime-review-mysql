b029274d364a6aaafb21349db4ffcc3f6acf84c0
kosuke <irohalove2525@gmail.com>
2023-06-18 22:34:31 +0900

    challenge production 4

--- entrypoint.prod.sh
+++ entrypoint.prod.sh
@@ -6,7 +6,7 @@ rm -f /myapp/tmp/pids/server.pid
 # bundle exec rails db:reset DISABLE_DATABASE_ENVIRONMENT_CHECK=1
 
 bundle exec rails db:migrate
-# bundle exec rails db:seed
+bundle exec rails db:seed
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
