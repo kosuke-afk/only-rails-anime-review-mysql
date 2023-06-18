4b189a409e8b087168a2cf064c0021cb24cb5092
kosuke <irohalove2525@gmail.com>
2023-06-18 21:06:42 +0900

    change entryponint.prod

--- entrypoint.prod.sh
+++ entrypoint.prod.sh
@@ -3,9 +3,10 @@ set -e
 
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /myapp/tmp/pids/server.pid
+bundle exec rails db:reset
 
-bundle exec rails db:migrate
-bundle exec rails db:seed
+# bundle exec rails db:migrate
+# bundle exec rails db:seed
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
