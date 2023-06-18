7d9ed98f5e4f5bba34aab684d80b49600351032c
kosuke <irohalove2525@gmail.com>
2023-06-19 03:00:11 +0900

    challenge production 11

--- entrypoint.prod.sh
+++ entrypoint.prod.sh
@@ -3,7 +3,7 @@ set -e
 
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /myapp/tmp/pids/server.pid
-bundle exec rails db:drop DISABLE_DATABASE_ENVIRONMENT_CHECK=1
+# bundle exec rails db:drop DISABLE_DATABASE_ENVIRONMENT_CHECK=1
 bundle exec rails db:migrate
 
 bundle exec rails db:seed
